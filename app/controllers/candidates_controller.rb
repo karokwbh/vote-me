24383cd0c9847480bc3d5bcda693664e3bd6efbf
Frank Huang <karokwbh2000@gmail.com>
2023-03-25 15:17:43 +0800

    add candidate insert into db

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -3,8 +3,20 @@ class CandidatesController < ApplicationController
   end
 
   def new
+    @candidate = Candidate.new
   end
 
   def create
+    @candidate = Candidate.new(candidate_params)
+    if @candidate.save
+      flash[:notice] = "Candidate created!"
+      redirect_to '/candidates'
+    else
+    end
+  end
+
+  private
+  def candidate_params
+    params.require(:candidate).permit(:name, :party, :age, :politics)
   end
 end
