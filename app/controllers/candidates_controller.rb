51c793ad55a2875301d5a3dd184442bf13a77fba
Frank Huang <karokwbh2000@gmail.com>
2023-03-25 16:44:46 +0800

    add index and show action add check candidate name

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,5 +1,10 @@
 class CandidatesController < ApplicationController
   def index
+    @candidates = Candidate.all
+  end
+
+  def show
+    @candidate = Candidate.find_by(id: params[:id])
   end
 
   def new
@@ -12,6 +17,10 @@ class CandidatesController < ApplicationController
       flash[:notice] = "Candidate created!"
       redirect_to '/candidates'
     else
+      # 並不是執行上面的 def new
+      # 而是借template new.html.erb 重新刷新
+      # 剛好new.html.erb是使用@candidate 所以資料會保存
+      render :new
     end
   end
 
